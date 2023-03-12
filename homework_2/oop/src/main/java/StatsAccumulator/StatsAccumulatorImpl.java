591a20c49b8198c97f1d4305bb15e589235908a5
Gonzik28 <gonina.al.vl@gmail.com>
2023-03-12 10:00:36 +0300

    init home work 2

---
+++ homework_2/oop/src/main/java/StatsAccumulator/StatsAccumulatorImpl.java
@@ -0,0 +1,49 @@
+package StatsAccumulator;
+
+public class StatsAccumulatorImpl implements StatsAccumulator {
+    private int value;
+    private int count = 0;
+    private int sum = 0;
+
+    private double avg = 0.0;
+    private int min = Integer.MIN_VALUE;
+    private int max = Integer.MAX_VALUE;
+
+    @Override
+    public void add(int value) {
+        this.value = value;
+        sum += value;
+        if (count == 0) {
+            min = value;
+            max = value;
+        } else if (value < min) {
+            min = value;
+        } else if (value > max) {
+            max = value;
+        }
+        count++;
+    }
+
+    @Override
+    public int getMin() {
+        return min;
+    }
+
+    @Override
+    public int getMax() {
+        return max;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public Double getAvg() {
+        if (count != 0) {
+            avg = Double.valueOf(sum) / Double.valueOf(count);
+        }
+        return avg;
+    }
+}
